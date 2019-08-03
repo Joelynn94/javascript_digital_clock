17f1b6c44381720670bc210fd3e142c5a0e9d709
Joseph Lynn <josephlynn94@gmail.com>
2019-08-03 12:41:26 -0400

    Add files via upload
    Added currentDate and styling

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 // reference to the clock div
 const clock = document.querySelector('.clock');
 
+
 // clock function 
 function tick(){
 
-    const date = new Date();
+    const time = new Date();
+    const currentDate = new Date(); 
 
-    let seconds = date.getSeconds();
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
+    let seconds = time.getSeconds();
+    let minutes = time.getMinutes();
+    let hours = time.getHours();
+    let suffix = "AM";
+    // displays current date
+    let date = currentDate.toDateString();
 
+    // adds suffix AM or PM
+    suffix = (hours === 0) ? hours - 12 : suffix;
+    suffix = (hours >= 12) ? "PM" : suffix;
+
+    // IF check using ternary operators 
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    hours = (hours < 10) ? "0" + hours : hours;
+
+    // html template8+
+    const html = `
+    <span>${hours}</span> :
+    <span>${minutes}</span> :
+    <span>${seconds}</span> ${suffix}   
+    <div>
+        <h6>${date}</h6>
+    </div>
+    `;
+
+    clock.innerHTML = html;
+
+} // ends tick() function
+
+// calls the tick function every second to update the time
+setInterval(tick, 1000);
+
+
+
+
+// Can use IF check instead of ternary operator
     // if(seconds < 10){
     //     seconds = "0" + seconds;
     // } 
@@ -21,21 +56,3 @@ function tick(){
     // if(hours < 10){
     //     hours = "0" + hours;
     // }
-
-    // IF check using ternary operators 
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    hours = (hours < 10) ? "0" + hours : hours;
-
-    const time = `
-    <span>${hours}</span> :
-    <span>${minutes}</span> :
-    <span>${seconds}</span>    
-    `;
-
-    clock.innerHTML = time;
-
-} // ends tick() function
-
-// calls the tick function every second to update the time
-setInterval(tick, 1000);
